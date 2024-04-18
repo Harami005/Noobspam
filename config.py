d015547acfe9e0bc318fe7e348dd57591a4e5692
Harami005 <161682442+Harami005@users.noreply.github.com>
2024-04-18 18:28:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 # VALUES REQUIRED FOR XBOTS
-API_ID = 
-API_HASH = ""
+API_ID = 18136872
+API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
@@ -27,7 +27,7 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5823219101").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5823219101,6555643597").split()))
 for x in OXYGEN:
     SUDO_USERS.append(x)
 OWNER_ID = int(getenv("OWNER_ID", default="5823219101"))
